66003d821360db7ebe02b7fe1ee6a99fd02cf155
ejyanaca <elvisyanacayo@gmail.com>
2025-07-01 20:50:05 -0500

    Modularizacion de las funciones imprime contactos y la tabla de contactos

---
+++ Proyecto-contacto/imprime.h
@@ -0,0 +1,11 @@
+#ifndef IMPRIME_H
+#define IMPRIME_H
+
+#include "leer_contacto.h"
+#include <iostream>
+#include <iomanip>
+
+void imprimeContacto(const contactoEmail &);
+void imprimeTabla(contactoEmail[],int);
+
+#endif
